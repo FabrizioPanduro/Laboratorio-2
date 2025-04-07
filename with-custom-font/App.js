49eb2adcc1db931d2e61188f11be47cd5ec6dd30
FabrizioPanduro <202012303@urp.edu.pe>
2025-04-07 01:35:34 +0000

    Laboratorio 2 PANDURO

--- with-custom-font/App.js
+++ with-custom-font/App.js
@@ -18,8 +18,8 @@ export default function App() {
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Platform Default</Text>
-      <Text style={{ fontFamily: "Inter-Black" }}>Inter Black</Text>
+      <Text>LABORATORIO 2</Text>
+      <Text style={{ fontFamily: "Inter-Black" }}>FABRIZIO PANDURO (Estilo de letra: Inter-Black)</Text>
     </View>
   );
 }
